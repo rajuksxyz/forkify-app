9a92c36f33b90d245011e8a5b025562ace4f61b2
rajuksxyz <rajuksxyz@gmail.com>
2024-01-14 12:54:24 +0530

    Initial Commit.

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,18 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+const showReceipe = async function(){
+  try{
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    console.log(res,data);
+  }catch(err){
+    alert(err)
+  }
+}
+
+showReceipe();
